b6320b1bef85dd7f94ffb1a0d3610e5146d8a589
Guillermo <guillermo.jimenez@ni.com>
2025-02-21 00:54:02 -0600

    Mapping Classes to the DB Tables

---
+++ src/main/java/com/platzi/platzimarket/persistence/entities/Compra.java
@@ -0,0 +1,71 @@
+package com.platzi.platzimarket.persistence.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "compras")
+public class Compra {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_compra")
+    private int idCompra;
+    @Column(name = "id_cliente")
+    private String idCliente;
+
+    private LocalDateTime fecha;
+    @Column(name = "medio_pago")
+    private String medioPago;
+    private String comentario;
+    private boolean estado;
+
+
+  public int getIdCompra() {
+    return idCompra;
+  }
+
+  public void setIdCompra(int idCompra) {
+    this.idCompra = idCompra;
+  }
+
+  public String getIdCliente() {
+    return idCliente;
+  }
+
+  public void setIdCliente(String idCliente) {
+    this.idCliente = idCliente;
+  }
+
+  public LocalDateTime getFecha() {
+    return fecha;
+  }
+
+  public void setFecha(LocalDateTime fecha) {
+    this.fecha = fecha;
+  }
+
+  public String getMedioPago() {
+    return medioPago;
+  }
+
+  public void setMedioPago(String medioPago) {
+    this.medioPago = medioPago;
+  }
+
+  public String getComentario() {
+    return comentario;
+  }
+
+  public void setComentario(String comentario) {
+    this.comentario = comentario;
+  }
+
+  public boolean isEstado() {
+    return estado;
+  }
+
+  public void setEstado(boolean estado) {
+    this.estado = estado;
+  }
+}
