3cf49b0fb69a212d4dab86a3de11ad0df3340859
Guillermo <guillermo.jimenez@ni.com>
2025-03-03 21:15:34 -0600

    Add @Repository annotation and more methods

--- src/main/java/com/platzi/platzimarket/persistence/ProductoRepository.java
+++ src/main/java/com/platzi/platzimarket/persistence/ProductoRepository.java
@@ -2,10 +2,12 @@ package com.platzi.platzimarket.persistence;
 
 import com.platzi.platzimarket.persistence.crud.ProductoCRUDRepository;
 import com.platzi.platzimarket.persistence.entities.Producto;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProductoRepository {
   private ProductoCRUDRepository productoCRUDRepository;
 
@@ -20,4 +22,16 @@ public class ProductoRepository {
   public Optional<List<Producto>> getEscasos(int cantidadStock){
     return productoCRUDRepository.findByCantidadStockLessThanAndEstado(cantidadStock, true);
   }
+
+    public Optional<Producto> getProducto(int idProducto){
+        return productoCRUDRepository.findById(idProducto);
+    }
+
+    public Producto save(Producto producto){
+        return productoCRUDRepository.save(producto);
+    }
+
+    public void delete(int idProducto){
+        productoCRUDRepository.deleteById(idProducto);
+    }
 }
